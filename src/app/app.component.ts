6b1fb0942f82f57ceb1d0887b29d0f62e9212244
Eya Somai <Eya.somai134t@gmail.com>
2020-11-15 11:35:49 +0100

    projet initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'sport2';
+  maDate = new Date(2019,9,23);
+  ville = "bizerte"}
